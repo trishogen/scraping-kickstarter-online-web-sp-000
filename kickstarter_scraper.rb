6462131a479b76813e5f3666af2097105f58aa17
Trish Ogen <patriciaogen@gmail.com>
2019-06-02 18:20:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,11 @@
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 end
+
+binding.pry
+
+create_project_hash
